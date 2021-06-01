3a42aedb0880a6e6276a1911ac83639ffa546905
tamaskis <tamas.a.kis@outlook.com>
2021-05-31 21:19:16 -0700

    New header format + example as live script.

--- c2d_euler.m
+++ c2d_euler.m
@@ -1,15 +1,10 @@
+%==========================================================================
+%
 % c2d_euler  Transforms a continuous transfer function to a discrete 
 % transfer function using the forward and backward Euler methods.
 %
-%   Hz = c2d_euler(Hs,T,'forward') returns the discrete transfer function
-%   "Hz" obtained by applying the forward Euler (i.e. forward difference)
-%   transformation to a continuous transfer function "Hs", where "T" is the
-%   sampling period.
-%
-%   Hz = c2d_euler(Hs,T,'backward') returns the discrete transfer function
-%   "Hz" obtained by applying the backward Euler (i.e. backward difference)
-%   transformation to a continuous transfer function "Hs", where "T" is the
-%   sampling period.
+%   Hz = c2d_euler(Hs,T,'forward')
+%   Hz = c2d_euler(Hs,T,'backward')
 %
 % See also c2d
 %
@@ -17,20 +12,23 @@
 % GitHub: https://github.com/tamaskis/c2d_euler-MATLAB
 %
 % See "DOCUMENTATION.pdf" for additional documentation and examples. 
-% Examples can also be found in EXAMPLES.m. Both of these files are 
+% Examples can also be found in EXAMPLES.mlx. Both of these files are 
 % included with the download.
 %
 % Copyright (c) 2021 Tamas Kis
-% Last Update: 2021-04-10
-
-
-
-%% FUNCTION
-
-% INPUT: Hs - continuous transfer function
-%        T - sampling period
-%        type - 'forward' or 'backward'
-% OUTPUT: Hz - discrete transfer function
+% Last Update: 2021-05-31
+%
+%--------------------------------------------------------------------------
+%
+% INPUTS:
+%   Hs      continous transfer function
+%   T       (1x1) sampling period
+%   type    'forward' or 'backward'
+%
+% OUTPUTS:
+%   Hz      discrete transfer function
+%
+%==========================================================================
 function Hz = c2d_euler(Hs,T,type)
     
     % symbolic variable for z;
